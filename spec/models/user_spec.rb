75869714f9795767849b6d1154ac01f5caff7065
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-26 21:43:02 +0900

    Faker 記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     it "nicknameが空だと登録できない" do
       @user.nickname = ""
-      user.valid?
+      @user.valid?
       expect(@user.errors.full_messages).to include("Nickname can't be blank")
     end
     it "emailが空では登録できない" do
